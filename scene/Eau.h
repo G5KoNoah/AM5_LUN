fa591920de340d901c5cb2d533b4d2d57f82b995
Ugo H <ugohong@gmail.com>
2025-10-16 11:34:14 +0200

    Corrections erreurs typo importantes

--- scene/Eau.h
+++ scene/Eau.h
@@ -16,7 +16,7 @@ public:
     Eau(std::string strShader, std::string strTexture1, std::string strTexture2, Transform tr, Entity* p); ///Constructeur deux textures
     Eau(std::string strShader, vec3 c, Transform tr, Entity* p); ///Constructeur couleur
 
-	void Eau::Draw(Orbiter* camera, Dirlight* dirLight, vector<PointLight*> pointLights) override;
+	void Draw(Orbiter* camera, Dirlight* dirLight, vector<PointLight*> pointLights) override;
 };
 
 #endif
--- scene/Plane.cpp
+++ scene/Plane.cpp
@@ -1,4 +1,4 @@
-#include "plane.h"
+#include "Plane.h"
 
 // Classe representant un plane
 
