b81557dde07f7ee187553a5bb2dd0a16f24c3b31
Léonard Benazeth-Lapierre <leonard.benazeth.lapierre@gmail.com>
2025-10-16 09:21:14 +0200

    Ajout d'un shader de depthMapping

---
+++ scene/shaders/depthShader.glsl
@@ -0,0 +1,26 @@
+#version 330 core
+
+#ifdef VERTEX_SHADER
+
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 lightSpaceMatrix;
+uniform mat4 model;
+
+void main()
+{
+    gl_Position = lightSpaceMatrix * model * vec4(aPos, 1.0);
+}  
+
+#endif
+
+
+
+#ifdef FRAGMENT_SHADER
+
+void main()
+{             
+    // gl_FragDepth = gl_FragCoord.z;
+}  
+
+#endif
