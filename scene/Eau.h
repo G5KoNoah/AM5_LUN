87ece2b2c6d6fe2f5bc3a28489102e82a09dc3ba
Léonard Benazeth-Lapierre <leonard.benazeth.lapierre@gmail.com>
2025-11-13 09:02:13 +0100

    Rajout de Eau.h

---
+++ scene/Eau.h
@@ -0,0 +1,22 @@
+#ifndef _EAU
+#define _EAU
+
+#include "Plane.h"
+
+
+/*
+    @class Eau
+	Classe cr�ant un Eau 3D
+*/
+
+class Eau : public Plane {
+
+public:
+    //Eau(std::string strShader, std::string strTexture1, Transform tr, Entity* p); ///Constructeur une seule texture
+    //Eau(std::string strShader, std::string strTexture1, std::string strTexture2, Transform tr, Entity* p); ///Constructeur deux textures
+    Eau(std::string strShader, vec3 c, Transform tr, Entity* p); ///Constructeur couleur
+
+	void Draw(Orbiter* camera, Dirlight* dirLight, vector<PointLight*> pointLights) override;
+};
+
+#endif
