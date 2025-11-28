e0f3810991a13b781227d3be3fd25c58fead1120
G5KoNoah <noah.hilaire@outlook.fr>
2025-11-28 16:10:21 +0100

    ajouts scene ile

--- scene/Scene.cpp
+++ scene/Scene.cpp
@@ -27,38 +27,28 @@ int Scene::init(){
 	base = new Entity();
 	// Creation d'une lumiere
 	dirLight = new Dirlight(vec3(0.2, 0.2, 0.2), vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0), Identity()* Translation(vec3(5.0,5.0,5.0)), base, vec3(-0.2f, -1.0f, -0.1f));
-	pointLights.push_back(new PointLight(vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), 1.0f, 0.09f, 0.032f, Translation(vec3(2.0f, 1.0f, 0.0f)), base));
+	//pointLights.push_back(new PointLight(vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), 1.0f, 0.09f, 0.032f, Translation(vec3(2.0f, 1.0f, 0.0f)), base));
     objects.push_back(new Sky("../shader/clouds.glsl", vec3(1.0, 1.0, 1.0), Identity(), base));
 	// LISTE DES OBJETS
-    //objects.push_back(new Cube("../tutos/multipleLights.glsl", vec3(0.5, 0.5, 0.5), Identity() * Translation(vec3(2.5, 0.0, 0.0)), base));
-	//objects.push_back(new Terrain("../tutos/tuto9_color.glsl", vec3(0.0f,1.0f,0.0f), Identity(), base));
-	//objects.push_back(new Eau("../tutos/eau2.glsl", vec3(0.0f, 0.0f, 1.0f), Identity(), base));
-
-	objects.push_back(new ObjectLoad(  "../shader/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity(), base, "../data/source/van.obj" ));
-    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/road/texture_road.jpg", "../data/road/texture_road.jpg", Identity(), base, "../data/road/road.obj"));
-    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/bridge/texture_bridge.jpg", "../data/bridge/texture_bridge.jpg", Identity(), base, "../data/bridge/bridge.obj"));
     objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/island/beton.jpg", "../data/island/beton.jpg", Identity(), base, "../data/island/island.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/Cottage_Dirt_Base_Color.png", "../data/Cottage_Dirt_Normal.png", Identity()*Translation(0.0f,0.3f,3.0f)*Scale(0.3), objects[1], "../data/Cottage_FREE.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/road/texture_road.jpg", "../data/road/texture_road.jpg", Identity() * Translation(-0.4f, 0.0f, 0.f) * Scale(0.36), objects[1], "../data/road/road.obj"));
+	objects.push_back(new ObjectLoad(  "../shader/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity() * Translation(0.0f, 0.3f, -3.0f) * Scale(0.5), objects[1], "../data/source/van.obj" ));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity() * Translation(-1.0f, 0.3f, -3.0f) * Scale(0.5), objects[1], "../data/source/van.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity() * Translation(1.0f, 0.3f, -3.0f) * Scale(0.5), objects[1], "../data/source/van.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/bridge/texture_bridge.jpg", "../data/bridge/texture_bridge.jpg", Identity() * Translation(5.0f, 0.3f, -4.0f)* RotationY(30.) * Scale(0.5), objects[1], "../data/bridge/bridge.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/boat_d.tga", "../data/boat_n.tga", Identity() * Translation(6.5f, 0.f, -5.5f) * RotationY(120.) * Scale(0.008), objects[1], "../data/boat.obj"));
+    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/lampadaire/lampadaire.png", "../data/lampadaire/lampadaire.png", Identity() * Translation(3.0f, 0.3f, 1.0f) * Scale(0.05), objects[1], "../data/lampadaire/lampadaire.obj"));
+    pointLights.push_back(new PointLight(vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), vec3(1.0, 1.0, 1.0), 1.0f, 0.045f, 0.0075f, Identity() * Translation(4.0f, 0.3f, 1.0f), base));
 
-    objects.push_back(new Terrain("../tutos/multipleLights.glsl", "../data/grass.jpg", "../data/grass_spec.jpg", Identity(), base));
+    //   objects.push_back(new Terrain("../tutos/multipleLights.glsl", "../data/grass.jpg", "../data/grass_spec.jpg", Identity(), base));
+ 
+ //   objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/banc_base.png", "../data/banc_normal", Identity() * Translation(2.0f, 2.0f, -2.0f) * Scale(0.5), objects[4], "../data/banc.obj"));
 
-	//objects.push_back(new ObjectLoad(  "../shader/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity(), base, "../data/source/van.obj" ));
-    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/Cottage_Dirt_Base_Color.png", "../data/Cottage_Dirt_Normal.png", Identity()*Translation(2.0f,2.0f,2.0f)*Scale(0.2), base, "../data/Cottage_FREE.obj"));
-    //objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/Cat_diffuse.jpg", "../data/Cat_bump.jpg", Identity() * Translation(-2.0f, 2.0f, 2.0f) * RotationX(-90.0f) * Scale(0.02), base, "../data/cat.obj"));
-    //objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/Wood_Tower_Col.jpg", "../data/Wood_Tower_Nor.jpg", Identity() * Translation(-2.0f, 2.0f, -2.0f) * Scale(0.5), base, "../data/watchTower.obj"));
-    //objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/fish.jpg", Identity() * Translation(-2.0f, 2.0f, -2.0f) * Scale(0.5), base, "../data/fish.obj"));
-    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/boat_d.tga", "../data/boat_n.tga", Identity() * Translation(-2.0f, 2.0f, -2.0f) * Scale(0.008), base, "../data/boat.obj"));
-    objects.push_back(new ObjectLoad("../shader/multipleLights.glsl", "../data/banc_base.png", "../data/banc_normal", Identity() * Translation(2.0f, 2.0f, -2.0f) * Scale(0.5), objects[4], "../data/banc.obj"));
-    //objects.push_back(new Terrain("../tutos/multipleLights.glsl", "../data/grass.jpg", "../data/grass_spec.jpg", Identity(), base));
-    //objects.push_back(new ObjectLoad("../tutos/multipleLights.glsl", "../data/textures/Material_BaseColor.png", "../data/textures/Material_Metallic.png", Identity()* Translation(vec3(2.0,0.0,0.0)), base, "../data/source/van.obj"));
-    //objects.push_back(new Cube("../tutos/tuto9_color.glsl", vec3(0.5, 0.5, 0.5), Identity() * Translation(vec3(2.5, 0.0, 0.0)), base));
-	//objects.push_back(new Plane("../tutos/multipleLights.glsl","../data/container2.png","../data/container2_specular.png", Identity(), base));
-	
-    //objects.push_back(new Arbre("../shader/multipleLights.glsl", Identity() * Translation(vec3(0.0, 1.0, 0.0)), base));
-    //objects.push_back(new Billboard("../shader/billboard.glsl", "../data/cloud.png", Identity() * Scale(10.), base));
-	Arbres * a = new Arbres("../shader/multipleLights.glsl", vec3(0.4, 0.25, 0.1), Identity(), base, (Terrain*)objects[1]);
-    for (unsigned int i = 0; i < 5; i++) {
-		objects.push_back(a->get_tree(i));
-    }
+	//Arbres * a = new Arbres("../shader/multipleLights.glsl", vec3(0.4, 0.25, 0.1), Identity(), base, (Terrain*)objects[1]);
+ //   for (unsigned int i = 0; i < 5; i++) {
+	//	objects.push_back(a->get_tree(i));
+ //   }
 
 
 	
@@ -341,7 +331,7 @@ int Scene::render(){
         prevTranslation = newTranslation;
         currentTranslation = newTranslation;
     }
-
+    
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
