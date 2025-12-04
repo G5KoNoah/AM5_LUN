31ce7efeb8db75769bbf37fee0fcb0b7ff5f47ca
G5KoNoah <noah.hilaire@outlook.fr>
2025-12-04 10:20:18 +0100

    placement point light

--- shader/multipleLights.glsl
+++ shader/multipleLights.glsl
@@ -90,7 +90,7 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     diffuse  *= attenuation;
     specular *= attenuation;
 
-    return (ambient + diffuse + specular) * vec3(1.,1.0,1.0);
+    return (ambient + diffuse + specular) ;
 }
 
 void main()
